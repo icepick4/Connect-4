508f570de294664786c4942d4913928ba3260069
Icepick <remi.jara@laposte.net>
2022-03-13 21:16:38 +0100

    last commit

--- Classes/board.py
+++ Classes/board.py
@@ -10,28 +10,39 @@ class Board:
     def drawBoard(self):
         #vertical
         for i in range(8):
-            pygame.draw.line(self.screen, (0,0,0), (self.x + i * self.x, self.y), (self.x + i * self.x, self.y * 8 - self.y), 5)
+            pygame.draw.line(self.screen, (0,0,0), (self.x + i * self.x + 2, self.y), (self.x + i * self.x + 2, self.y * 8 - self.y), 2)
+            pygame.draw.line(self.screen, (50,50,50), (self.x + i * self.x, self.y), (self.x + i * self.x, self.y * 8 - self.y), 4)
+            pygame.draw.line(self.screen, (0,0,0), (self.x + i * self.x - 2, self.y), (self.x + i * self.x - 2, self.y * 8 - self.y), 2)
         #horizontal
         for i in range(7):
-            pygame.draw.line(self.screen, (0,0,0), (self.x, self.y + i * self.y), (self.x * 9 - self.x, self.y + i * self.y), 5)
-    
+            pygame.draw.line(self.screen, (0,0,0), (self.x, self.y + i * self.y + 2), (self.x * 9 - self.x, self.y + i * self.y + 2), 2)
+            pygame.draw.line(self.screen, (50,50,50), (self.x, self.y + i * self.y), (self.x * 9 - self.x, self.y + i * self.y), 4)
+            pygame.draw.line(self.screen, (0,0,0), (self.x, self.y + i * self.y - 2), (self.x * 9 - self.x, self.y + i * self.y - 2), 2)
     def drawPawn(self, game):
         pawns = game.grid
         for i in range(game.rows):
             for j in range(game.columns):
                 if pawns[i][j] == 1:
+                    pygame.draw.circle(self.screen, (180,0,0), (self.x + j * self.x + self.x /2,self.y + i * self.y + self.y /2), 55, 5)
                     pygame.draw.circle(self.screen, (255,60,60), (self.x + j * self.x + self.x /2,self.y + i * self.y + self.y /2), 50)
                 elif pawns[i][j] == 2:
+                    pygame.draw.circle(self.screen, (160,150,5), (self.x + j * self.x + self.x /2,self.y + i * self.y + self.y /2), 55, 5)
                     pygame.draw.circle(self.screen, (255,215,0), (self.x + j * self.x + self.x /2,self.y + i * self.y + self.y /2), 50)          
             #draw a pawn
 
     def inBoard(self,click):
         return self.x <= click[0] < self.x * 8 and self.y <= click[1] < self.y * 7
 
+
     def seePawn(self, col, game, color):
         ctr = -1
         for i in range(game.rows):
             if game.grid[i][col] == 0:
                 ctr+=1
+        if color == (255,60,60):
+            borderColor = (180,0,0)
+        else:
+            borderColor = (160,150,5)
+        pygame.draw.circle(self.screen, borderColor, (self.x + col * self.x + self.x /2,self.y + ctr * self.y + self.y /2), 55, 5)
         pygame.draw.circle(self.screen, color, (self.x + col * self.x + self.x /2,self.y + ctr * self.y + self.y /2), 50)
 
--- main.py
+++ main.py
@@ -4,27 +4,32 @@ try:
     from pygame.locals import *
 except:
     print("Vous n'avez pas téléchargé le module pygame ! \n Téléchargez le avec la commande ci-contre : pip install pygame")
+
 from Classes.board import Board
-from random import randint
 
 while playing:
     screen.fill((30,144,255))
+    pygame.draw.rect(screen, (45,45,45), (0,height - height // 8, width, height))
+
     board = Board(width, height, screen)
     posMouse = pygame.mouse.get_pos()
+    posX = posMouse[0]
+    posY = posMouse[1]
+    case = int((posX % width // 7) // (width/7 % width / 9) - 1)
     for event in pygame.event.get():
         if event.type == QUIT:
             playing = False
         elif event.type == pygame.MOUSEBUTTONDOWN and event.button == 1 :
-            click = event.pos
-            clickX = click[0]
-            clickY = click[1]
-            case = int((clickX % width // 7) // (width/7 % width / 9) - 1)
+            # click = event.pos
+            # posX = click[0]
+            # posY = click[1]
+            #case = int((posX % width // 7) // (width/7 % width / 9) - 1)
             #on presse le bouton close
-            if width-180 < clickX < width and 10 < clickY < 60:
+            if width-180 < posX < width and 10 < posY < 60:
                 playing = False
-            if board.inBoard(click) and not game.fullColumn(case) and inGame:
+            if board.inBoard(posMouse) and not game.fullColumn(case) and inGame and case !=7 and case !=-1:
                 inGame = True
-                #caseX = int((clickX % width // 7) // 30.14 - 1)
+                #caseX = int((posX % width // 7) // 30.14 - 1)
                 game.play(case, player)
                 if player == 1:
                     player = 2
@@ -33,13 +38,13 @@ while playing:
             if not inGame:
                 inGame = True
                 game.resetGrid()
-    if board.inBoard(posMouse) and inGame:
+    if board.inBoard(posMouse) and inGame and case !=7 and case !=-1 and not game.fullColumn(case):
         posX = event.pos[0]
-        case = int((posX % width // 7) // (width/7 % width / 9) - 1)
+        #case = int((posX % width // 7) // (width/7 % width / 9) - 1)
         if player == 1:
-            board.seePawn(case, game, (255,60,60,128))   
+            board.seePawn(case, game, (255,60,60))   
         else:
-            board.seePawn(case, game, (255,215,0,128))  
+            board.seePawn(case, game, (255,215,0))  
             
     board.drawPawn(game)
     status = game.win()
