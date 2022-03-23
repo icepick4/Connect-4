81eeab5ce58010a7a937aacdfbe26603ab4ac2d1
Rémi <82316285+icepick4@users.noreply.github.com>
2022-03-23 16:58:50 +0100

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='connect-4',
+      version='1.0',
+      description='connect 4 game',
+      author='Rémi JARA',
+      author_email='remi.jara4@gmail.com',
+      packages=['pygame'],
+     )
